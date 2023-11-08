9f4c09079799cc13f5190892d172102f066dacc3
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-08 12:14:24 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-//your JS code here. If required.
-//your JS code here. If required.
-let a = document.getElementById("sizeInfo");
-function showhw() {
-	let x = window.innerWidth;
-	let y = window.innerHeight;
-	a.innerHTML = "Width: " + x + " and Height: " + y;
-}
-showhw();
-window.addEventListener('resize', showhw);
+
