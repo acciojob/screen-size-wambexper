d0bb57d36559d2b0b2fd06fb3bffadd5d7ad0882
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-14 16:41:08 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const calc = () => {
+	document.querySelector("h1")
+   .textContent = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`
+}
 
+window.addEventListener("resize", calc);
+calc()
